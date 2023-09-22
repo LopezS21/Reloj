e8194f57985a7f4afa3065d097ef558b454c7d08
LopezS7 <samir@HPEnvyX360>
2023-09-22 11:55:42 -0600

    Emcapsulamos la variables del modelo.

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -5,9 +5,7 @@ package Model;
  */
 public class Model {
     
-    public int hours;
-    public int minuts;
-    public int seconds;
+    private int hours, minuts, seconds;
 
     public Model() {
     }
@@ -18,46 +16,29 @@ public class Model {
         this.seconds = seconds;
     }
 
-    /**
-     * @return the hours
-     */
     public int getHours() {
         return hours;
     }
 
-    /**
-     * @param hours the hours to set
-     */
     public void setHours(int hours) {
         this.hours = hours;
     }
 
-    /**
-     * @return the minuts
-     */
     public int getMinuts() {
         return minuts;
     }
 
-    /**
-     * @param minuts the minuts to set
-     */
     public void setMinuts(int minuts) {
         this.minuts = minuts;
     }
 
-    /**
-     * @return the seconds
-     */
     public int getSeconds() {
         return seconds;
     }
 
-    /**
-     * @param seconds the seconds to set
-     */
     public void setSeconds(int seconds) {
         this.seconds = seconds;
     }
-    
+
+  
 }
